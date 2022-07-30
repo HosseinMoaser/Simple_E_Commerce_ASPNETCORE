c39efb19cb486fb24bf82c8ee7030b609a23e2d9
Hossein Moaser <moaser.hossein@gmail.com>
2022-07-30 14:07:34 +0430

    Create Project And Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A Simple E-Commerce web application using Asp.Net
+Hossein Moaser
+Gmail: moaser.hossein@gmail.com
---
+++ Simple_E_Commerce/Simple_E_Commerce.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Simple_E_Commerce.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
